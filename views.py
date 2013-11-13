425564d6519656d2488ec4fe2622cbe546172cfb
Andrea D'Este <andrea.deste@redomino.com>
2013-11-13 20:04:24 +0400

    adds next variable to context

--- views.py
+++ views.py
@@ -1,13 +1,17 @@
 # Create your views here.
 
 from django.template.context import RequestContext
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 from django.http import HttpResponse
 
 def login(request):
     context = RequestContext(request)
-    return render(request, 'lfs_facebook/login.html', context)
+#    import pdb; pdb.set_trace();
+    if request.REQUEST.get('next') != None:
+	    return render_to_response('lfs_facebook/login.html', {"my_next": request.REQUEST.get('next')}, context)
+    
+    return render_to_response('lfs_facebook/login.html', context)
 
 def text_try(request):
     return HttpResponse("Hello, world. You're at the poll index.")
