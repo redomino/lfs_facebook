3f1da237aad788207b852a5339dc36d36f13769e
Andrea D'Este <andrea.deste@redomino.com>
2013-11-26 17:19:57 +0400

    clean by useless functions

--- views.py
+++ views.py
@@ -3,8 +3,6 @@
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 
-from django.http import HttpResponse
-
 def login(request):
     context = RequestContext(request)
 #    import pdb; pdb.set_trace();
@@ -12,6 +10,3 @@ def login(request):
 	    return render_to_response('lfs_facebook/login.html', {"my_next": request.REQUEST.get('next')}, context)
     
     return render_to_response('lfs_facebook/login.html', context)
-
-def text_try(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
