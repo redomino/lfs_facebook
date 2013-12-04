42b7fc73923df924bb566e5a106b6f345eecb790
root <root@gd-f-6>
2013-12-04 17:56:22 +0100

    update

--- views.py
+++ views.py
@@ -5,12 +5,11 @@ from django.shortcuts import render_to_response
 
 def login(request):
     context = RequestContext(request)
-    import pdb; pdb.set_trace()
+
     if request.REQUEST.get('next') != None:
-        #assolutamente da modificare
         if request.REQUEST.get('next') == u'/product-form-dispatcher':
             return render_to_response('lfs_facebook/login.html', {"next": u'/cart'}, context)
         else:
-            return render_to_response('lfs_facebook/login.html', {"next": request.REQUEST.get('next')}, context)
-        
+            return render_to_response('lfs_facebook/login.html', {"next": request.REQUEST.get('next')}, context)        
+
     return render_to_response('lfs_facebook/login.html', context)
